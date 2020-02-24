f510beb52d37f057e6ec4b64806c32dffa4a4681
brojass <brojas61@gmail.com>
2020-02-24 15:23:50 -0300

    modified data types and header when an email it's send

--- aom_channels_monitor.py
+++ aom_channels_monitor.py
@@ -4,11 +4,13 @@ import smtplib
 from email.message import EmailMessage
 
 EMAILS_TO_SEND = ['brojas@gemini.edu']
+CHANNELS_LIST = ['aom:health']
+# CHANNELS_LIST = ['ws:present', 'ec:present']
 
 
 def on_conn_change(pvname=None, conn=None, **kws):
     """
-    Function that detect and it's triggered when the PV state connection change
+    Function that detect and it's triggered when the PV connection status changed.
     :param pvname: the name of the PV
     :type pvname: str
     :param conn: the connection status
@@ -16,27 +18,33 @@ def on_conn_change(pvname=None, conn=None, **kws):
     :param kws: additional keyword/value arguments
     :type kws: str
     """
-    print(kws)
 
     if not conn:
-        email_content = 'PV state connection change!', pvname, 'connected:', conn
+        email_content = "\n" + 'Channel ' + pvname + ' connection status changed! to disconnected.'
+        send_email(email_content, pvname)
         print(email_content)
-        send_email(email_content)
+    else:
+        print(kws)
+        print(pvname)
+        print(conn)
 
 
-def send_email(content):
+def send_email(content, header):
     """
     Function that send the email of the channel disconnection to a specifics persons.
+    :param header: The channel name
+    :type header: str
     :param content: The content of the message
-    :type content: tuple
+    :type content: str
     """
     for email in EMAILS_TO_SEND:
         print('Email send to ' + email)
         msg = EmailMessage()
-        msg['Subject'] = 'Timeout detected on channels'
+        msg['Subject'] = 'Timeout connection detected on ' + header
         msg['From'] = 'brojas@gemini.edu'
         msg['To'] = email
-        message = content
+        note = '\n' + '\n' + 'NOTE: Please check the AOM channels.'
+        message = content + note
         msg.set_content(message)
         s = smtplib.SMTP('localhost')
         s.send_message(msg)
@@ -49,10 +57,8 @@ if __name__ == '__main__':
     # export EPICS_CA_ADDR_LIST=172.17.65.100
 
     # set up the PV
-    p = epics.PV('aom:health', connection_callback=on_conn_change)
-    # print('First value:', p.get())
-
-    p.get()
+    for name in CHANNELS_LIST:
+        p = epics.PV(name, connection_callback=on_conn_change)
 
     while True:
         epics.poll(evt=1.e-5, iot=0.1)
